77c02ed4dca911b1c8886a390ee49deffffe5ee9
sorrowlol <sorrowlol@outlook.com>
2021-07-11 20:43:31 +0200

    added cats

---
+++ js.js
@@ -0,0 +1,8 @@
+let calculator = document.querySelectorAll('.buttons')
+
+/*calculator.forEach((x) => {
+    x.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = `rgba(${Math.floor(Math.random()*240+80)}, ${Math.floor(Math.random()*240+80)}, ${Math.floor(Math.random()*0)}, 1)`;
+        })
+    });
+*/
